f191134f9fe7385f0b6d1c15d69f49909d4c1f58
JackStat <tyler.hunt@utah.edu>
2014-02-20 13:29:23 -0700

    Updated code and programs to fit modern R/shiny style.

--- server.R
+++ server.R
@@ -1,25 +1,13 @@
-generator = function(n, pop.mean , pop.sd, conf.lvl) {
-  plot(NULL, xlim = c(pop.mean-pop.sd,pop.mean+pop.sd),ylim=c(0,100),yaxt = 'n',xlab= (conf.lvl), , ylab=(n), main="Confidence Intervals of 100 Samples")
-  abline(v=pop.mean, col='black')
-  
-  for (i in 1:100){
-    x<-rnorm(n, mean = pop.mean, sd = pop.sd)
-    test<-t.test(x,conf.level=conf.lvl)
-    interval<-test$conf.int
-    if(pop.mean>interval[1] & pop.mean<interval[2])
-    {lines(c(interval[1],interval[2]),c(i,i), lwd=2,col='black')}
-    else
-    {lines(c(interval[1],interval[2]),c(i,i), lwd=2,col='red' )} 
-  }
-  
-}
 
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
   
-  output$conf.plot<-reactivePlot(function(){
+  output$conf.plot<-renderPlot(function(){
+    
     generator(input$nsamp, input$mean, input$variance, (input$conf.level/100))
+    
   })
+  
   
 })
 
